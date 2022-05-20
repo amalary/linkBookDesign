3ed17d4f828c26e3fcb6167764b4e99193c91df7
Anthony Malary <amalary@bu.edu>
2022-05-20 15:16:39 -0700

    Progressing on nav bar

--- link-book-design/src/components/topbar/Topbar.jsx
+++ link-book-design/src/components/topbar/Topbar.jsx
@@ -1,42 +1,58 @@
-import "./topBar.css" 
-import {SearchSharp, Person, Chat,NotificationsSharp } from "@mui/icons-material"
+import "./topBar.css"
+import {SearchSharp, PersonSharp,ChatSharp, NotificationsSharp} from "@mui/icons-material"
 
 export default function Topbar() {
   return (
-    <div className="topBarContainer">
-      <div className="topBarLeft">
-        <span className="logo">LinkBook</span>
-      </div>
-      <div className="topBarCenter">
-        <div className="searchBar">
-          <SearchSharp/>
-          <input placeholder="Search" className="searchInput" />
+    <div className = "topBarContainer">
+        
+        <div className="topBarLeft">
+
+
+          <span className="logo">LinkBook</span>
+        </div>
+
+
+        <div className="topBarCenter">
+
+          <div className="searchBar">
+            <SearchSharp className = "searchIcon"/>
+            
+            <input placeholder = "Search" className="searchInput"/>
+          </div>
+
 
         </div>
-      </div>
-      <div className="topBarRight">
 
-        <div className="topBarLinks">
-          <span className="topBarLink">Home Page</span>
-          <span className="topBarLink">Timeline</span>
-        </div>
-        <div className="topBarIcons">
-          <div className="topBarIconItem">
-            <Person/>
-            <span className="topBarIconCounter">2</span>
-          </div>
-          <div className="topBarIconItem">
-            <Chat/>
-            <span className="topBarIconCounter">1</span>
-          </div>
-          <div className="topBarIconItem">
-            <NotificationsSharp/>
-            <span className="topBarIconCounter">3</span>
-          </div>
-        </div>
-      </div>
-        {/* <img src="/assets/profilePictures/matheus-ferrero.jpg" alt="" className= "topBarImg"/> */}
+        <div className="topBarRight">
 
-    </div>
+          <div className="topBarLinks">
+            <span className="topBarLink">Homepage</span>
+            <span className="topBarLink">Timeline</span>
+
+
+          </div>
+
+          <div className="topBarIcons">
+            <div className="topBarIconItem">
+              <PersonSharp/>
+              <span className="topBarIconBadge">1</span>
+            </div>
+            <div className="topBarIconItem">
+              <ChatSharp/>
+              <span className="topBarIconBadge">4</span>
+            </div>
+            <div className="topBarIconItem">
+              <NotificationsSharp/>
+              <span className="topBarIconBadge">3</span>
+            </div>
+          </div>
+          <img src="assets/profilePictures/matheus-ferrero.jpg" alt="" className="topBarImg" />
+
+
+        </div>
+        
+        
+        
+        </div>
   )
 }
