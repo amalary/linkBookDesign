55e4a954934bf6b035e6982106143215ed0aa0a4
Anthony Malary <amalary@bu.edu>
2022-05-25 17:22:41 -0700

    ALmost finish sidebar

--- link-book-design/src/components/sidebar/Sidebar.jsx
+++ link-book-design/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,60 @@
 import '../sidebar/sidebar.css'
+import {RssFeedSharp,ChatSharp,VideocamSharp,GroupsSharp,BookmarkSharp,HelpSharp,WorkSharp,EventAvailableSharp,SchoolSharp} from "@mui/icons-material"
 
 export default function Sidebar() {
 
 return (
     <div className='sidebar'>
         
-        Sidebar
+       <div className="sidebarWrapper">
+          <ul className="sidebarList">
+              <li className="sidebarListItem">
+                  <RssFeedSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Feed</span>
+              </li>
+              <li className="sidebarListItem">
+                  <ChatSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Chat</span>
+              </li>
+              <li className="sidebarListItem">
+                  <VideocamSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Videos</span>
+              </li>
+              <li className="sidebarListItem">
+                  <GroupsSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Groups</span>
+              </li>
+              <li className="sidebarListItem">
+                  <BookmarkSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Bookmarks</span>
+              </li>
+              <li className="sidebarListItem">
+                  <HelpSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Questions</span>
+              </li>
+              <li className="sidebarListItem">
+                  <WorkSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Jobs</span>
+              </li>
+              <li className="sidebarListItem">
+                  <EventAvailableSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Events</span>
+              </li>
+              <li className="sidebarListItem">
+                  <SchoolSharp className='sidebarIcon'/>
+                  <span className="sidebarListItemText">Courses</span>
+              </li>
+          </ul>
+          <button className='sidebarButton'>Show More</button>
+          <hr className='sidebarHr'/>
+          <ul className="sidebarFriendList">
+              <li className="sidebarFriend">
+                  <img src="assets/profilePictures/maryjaneWatson.jpg" alt="" />
+                  <span className='sidebarFriendName'>Maryjane Watson</span>
+              </li>
+          </ul>
+         
+       </div>
     
         
     </div>
